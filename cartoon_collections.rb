0d34c74e2845516a6e5b59ccba7705ed6e916bf1
Jordan Kline <jordan.kline@gmail.com>
2018-01-06 01:13:22 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,5 +13,5 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.map { |el| cheese_types.include?(el) }
+  array.any? { |el| cheese_types.include?(el) }
 end
