47c5b5d9b948d59f34e5f70c83ac223018fa1c80
Jordan Kline <jordan.kline@gmail.com>
2018-01-06 01:10:48 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,6 @@ def long_planeteer_calls(array)
   array.any? {|e| e.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
